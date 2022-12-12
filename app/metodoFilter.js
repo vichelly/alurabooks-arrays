da3baa0eb353f5e2fc000961dcd63d6c12cdffb7
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-12-12 10:18:08 -0300

    att

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,4 +1,13 @@
+const botoes = document.querySelectorAll('.btn')
 
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
+
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
+}
 
 //filter
 //a função retorna em um novo array os objetos que suprem as necessidades passadas
